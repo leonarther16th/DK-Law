867b5ed96ab65f516d04b64400796f46a9b807d9
Moe Katib <Moe.Katib@gmail.com>
2015-11-05 21:36:11 -0500

    Changed the review mode, changed the left tabs

---
+++ javascripts/compareVersions.js
@@ -0,0 +1,38 @@
+var comapreEngine = function(){
+
+	//var selectedVersion;
+	var files = document.getElementsByClassName('file-link');
+
+
+	$.each(files, function(i, v){
+		$(v).click(function(){
+
+			var versionWells = $('.not-selected-version-well');
+
+			selectedVerstion = this.children.item(0).textContent;
+
+			$(this).toggleClass("selected-version");
+
+			/* make the well shack and chabge its text */
+			versionWells.toggleClass('shake-little shake-constant selectable');
+			versionWells.text('Click here to add!');
+
+            // adding a click event on the well
+			versionWells.click(function(){
+				$(this).text('Version ' + selectedVerstion + ' has been selected!');
+				$(this).removeClass('not-selected-version-well');
+				$(this).addClass('selected-version-well');
+                $(this).unbind( 'click' );
+				versionWells.removeClass('shake-little shake-constant selectable');
+                $('.not-selected-version-well').unbind('click');
+                $('.not-selected-version-well').text('Nothing selected yet!!');
+
+			})
+		});
+	});
+
+
+
+};
+
+comapreEngine();
